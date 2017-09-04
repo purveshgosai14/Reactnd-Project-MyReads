0d1d5be363d1e4ea6ba3638346dac6994079561e
Purvesh Gosai <puri.ui369@gmail.com>
2017-09-03 23:20:14 -0500

    Add search bar code
    Added search bar code with array for shelf and mapped to shelf value on change.

--- src/components/search.js
+++ src/components/search.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-
-class Search extends React.Component {
+class Search extends Component {
+    handleSelectChange = (e) => {
+        e.preventDefault();
+        console.log("curent", this.props);
+        if (this.props.updateShelf) {
+            this.props.updateShelf(this.props.book, e.target.value)
+        }
+    };
     render() {
+        const {book} = this.props;
+        const shelf = [
+            {text: 'Currently Reading', value: 'currentlyReading'},
+            {text: 'Want to Read', value: 'wantToRead'},
+            {text: 'Read', value: 'read'},
+            {text: 'None', value: 'none'},
+        ];
         return (
-<div className="search-books-bar">
-    <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-    <div className="search-books-input-wrapper">
-        <input type="text" placeholder="Search by title or author"/>
-    </div>
-</div>
+            <div className={`book-shelf-changer book-shelf-changer-${book.shelf}`}>
+                <select defaultValue={book.shelf !== undefined ? book.shelf : ""} onChange={this.handleSelectChange}>
+                    <option value="" disabled>Move to...</option>
+                    {shelf.map((shelf) => (
+                        <option key={shelf.value} value={shelf.value}>{shelf.text}</option>
+                    ))}
+                </select>
+            </div>
         )
     }
 }
 
-export default Search
-
+export default Search;
