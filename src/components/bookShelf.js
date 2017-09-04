f191aa33c39085ebded187c1e55e83ab3393a7cb
Purvesh Gosai <puri.ui369@gmail.com>
2017-09-03 23:17:21 -0500

    Fix: Remove jsx and add require jsx for book shelf
    Added jsx for book shelf with title for updating state and remove some jsx.

--- src/components/bookShelf.js
+++ src/components/bookShelf.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import BooksGrid from'./bookGrid'
+import React, { Component } from 'react'
+import BooksGrid from './BooksGrid'
 
-class BookShelf extends React.Component {
-        render() {
-            return (
-                <div className="bookshelf">
-                    <h2 className="bookshelf-title">Currently Reading</h2>
-                    <div className="bookshelf-books">
-                        <BooksGrid/>
-                    </div>
+class BookShelf extends Component {
+
+    render(){
+        const { title, books, updateShelf } = this.props
+
+        return(
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">{title}</h2>
+                <div className="bookshelf-books">
+                    <BooksGrid books={books} updateShelf={updateShelf}/>
                 </div>
-            )
-        }
+            </div>
+        )
+    }
 }
 
 export default BookShelf
