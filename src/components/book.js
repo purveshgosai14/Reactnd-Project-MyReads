e2db6d5287b93b200b3c0c83efbfaefa6fb1de51
Purvesh Gosai <puri.ui369@gmail.com>
2017-09-03 23:04:44 -0500

    remove unused jsx
    Remove unused jsx and added with require jsx for book and book cover.

--- src/components/book.js
+++ src/components/book.js
@@ -1,23 +1,27 @@
-import React,{ Component } from 'react';
+import React, { Component } from 'react';
+import Search from './Search';
 
+class Book extends Component {
+render(){
+    const { book, updateShelf } = this.props;
 
-class Book extends React.Component {
-        render() {
-            return (
-                <div className="book">
-                    <div className="book-top">
-                        <div className="book-cover" style={{
-                            width: 128,
-                            height: 193,
-                            backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")'
-                        }}></div>
-                        <Options/>
+    return(
+        <li>
+            <div className="book">
+                <div className="book-top">
+                    <div className="book-cover"
+                         style={{ width: 128, height: 193,
+                             backgroundImage: book.imageLinks ? `url(${book.imageLinks.thumbnail})` : ""
+                         }}>
                     </div>
-                    <div className="book-title">To Kill a Mockingbird</div>
-                    <div className="book-authors">Harper Lee</div>
+                    <Search book={book} updateShelf={updateShelf}/>
                 </div>
-            )
-        }
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.authors ? book.authors.join(", ") : ""}</div>
+            </div>
+        </li>
+         )
     }
+}
 
-export default Book
+export default Book;
